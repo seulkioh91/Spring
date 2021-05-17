3e2c1c19ae4abf74b8da02f898f922ee9c4b5392
seulkioh91 <78770899+seulkioh91@users.noreply.github.com>
2021-05-17 17:21:52 +0900

    210517class
    every project is not working.

---
+++ Exam_MyBatis/src/main/java/com/study/springboot/MyController.java
@@ -0,0 +1,27 @@
+package com.study.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.study.springboot.jdbc.IMyUserDao;
+
+@Controller
+public class MyController {
+	
+	@Autowired
+	private IMyUserDao userDao;
+	
+	@RequestMapping("/")
+	public @ResponseBody String root() throws Exception{
+		return "MyBatis 사용하기";
+	}
+	
+	@RequestMapping("/user")
+	public String userlistPage(Model model) {
+		model.addAttribute("users", userDao.getUser());
+		return "userlist";
+	}
+}
