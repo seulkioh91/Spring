36d1b3fa6d7d83cbdee9a279dbe42dbdb59b58e8
seulkioh91 <78770899+seulkioh91@users.noreply.github.com>
2021-06-07 17:20:01 +0900

    210607class

---
+++ home0607/src/main/java/com/example/home0607/Home0607Application.java
@@ -0,0 +1,13 @@
+package com.example.home0607;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Home0607Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Home0607Application.class, args);
+	}
+
+}
---
+++ home0607/src/test/java/com/example/home0607/Home0607ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.home0607;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Home0607ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
